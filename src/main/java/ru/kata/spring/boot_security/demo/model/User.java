af4e337181fb45c184406b95c23cf0b71f15beb8
ichurs <chursinov_ivan@mail.ru>
2023-02-13 23:38:44 +0300

    Entity Username: unique = true

--- src/main/java/ru/kata/spring/boot_security/demo/model/User.java
+++ src/main/java/ru/kata/spring/boot_security/demo/model/User.java
@@ -24,7 +24,7 @@ public class User implements Serializable, UserDetails {
     @Column(name = "lastname")
     private String lastname;
 
-    @Column(name = "username")
+    @Column(name = "username", unique = true)
     private String username;
 
     @Column(name = "password")
