9831e92016678843b164e9b90afe086b0231e4c1
ichurs <chursinov_ivan@mail.ru>
2023-03-03 14:27:10 +0300

    3.1.3 Исправление замечаний от 03.mar.23

--- src/main/java/ru/kata/spring/boot_security/demo/controller/UsersController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UsersController.java
@@ -4,28 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import ru.kata.spring.boot_security.demo.model.Role;
-import ru.kata.spring.boot_security.demo.model.User;
-import ru.kata.spring.boot_security.demo.service.RoleService;
 import ru.kata.spring.boot_security.demo.service.UsersService;
-
 import java.security.Principal;
-import java.util.Set;
 
 
 @Controller
 public class UsersController {
 
     private final UsersService usersService;
-    private final RoleService roleService;
 
     @Autowired
-    public UsersController(UsersService usersService, RoleService roleService) {
+    public UsersController(UsersService usersService) {
         this.usersService = usersService;
-        this.roleService = roleService;
     }
 
     @GetMapping("/login")
@@ -35,8 +25,7 @@ public class UsersController {
 
     @GetMapping("/user")
     public String getUserPage(Model model, Principal principal) {
-        Long id = usersService.getUserByUsername(principal.getName()).getId();
-        model.addAttribute("user", usersService.getUserById(id));
+        model.addAttribute("user", usersService.getUserByUsername(principal.getName()));
         return "user";
     }
 }
